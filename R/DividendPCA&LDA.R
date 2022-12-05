651b82ab68ae7327b339ebc9ea4f4b89a88eb15c
Ravish Kamath <ravishkamath1996@gmail.com>
2022-12-04 20:05:49 -0500

    Uploading Project file to github

---
+++ R/DividendPCA&LDA.R
@@ -0,0 +1,242 @@
+ARTofR::xxx_title2('4630 Project: Analysis of Dividend Data-set')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                4630 Project: Analysis of Dividend Data-set               ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Libraries ---------------------------------------------------------------
+library(psych)
+library(devtools)
+Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true")
+install_github("vqv/ggbiplot")
+library(ggbiplot)
+library(MASS)
+library(pROC)
+
+# Data set ----------------------------------------------------------------
+
+df=read.csv(file.choose(), header = T)
+df$dividend = factor(df$dividend)
+str(df)
+View(df)
+
+# Understanding the Data --------------------------------------------------
+
+# Our independent variables are as follows:
+
+##### fcfps: Free cash flow per share (in $) 
+
+# Definition: A measure of a company's financial flexibility that is determined 
+# by dividing free cash flow by the total number of shares outstanding. This 
+# measure serves as a proxy for measuring changes in earnings per share. 
+# Ideally a business will generate more cash flow than is required for operational
+# expenses and capital expenditures. If they do, the metric below will increase,
+# as the numerator grows holding shares outstanding constant. It is a positive sign
+# for an increase in free cash flow to outstanding shares value, and is viewed that 
+# the company is regarded as improving prospects and more financial & operational
+# flexibility
+
+##### earnings_growth: Earnings growth: in the past year (in %)
+
+# Definition: The change in an entity's report net income over a period of time.
+# The measure is usually a period-to-period comparison. The concept can also be
+# used to estimate growth in a future period over the current period. A high 
+# level of earnings growth is more likely to drive up the market price of a 
+# company's stock. Conversely, if the earnings growth rate declines, this can 
+# trigger a sell off by investors that drive down the stock price. 
+
+###### de: Debt to Equity ratio
+
+# Definition: This is used to evaluate a company's financial leverage and is 
+# calculated by dividing a company's total liabilities by its shareholder equity.
+# This is an important metric in corporate finance. It is a measure of the degree 
+# to which a company is financing its operations with debt rather than its own 
+# resources.D/E varies by industry and are best used to compare direct competitors.
+# A higher D/E ratio suggests more risk, while low may indicate the business is 
+# not taking advantage of debt financing to expand. 
+
+
+###### mcap: Market Capitalization of the stock 
+
+# Definition: Refers to the total dollar market value of a company's outstanding
+# shares of stock. The investment community uses this figure to determine a 
+# company's size instead of sales or total asset figures. In an acquisition, 
+# the market cap is used to determine whether a takeover candidate represents a 
+# good value or not to the acquirer.
+
+
+
+##### current_ratio: Current Ratio (or Current Assets/Current Liabilities)
+
+# Definition: A Liquidity ratio that measures a company's ability to pay short 
+# term obligations or those due within one year. It tells investors and analyst
+# how a company can maximize the current assets on its balance sheet to satisfy
+# its current debt and other payables. 
+
+# A current ratio that is in line with the industry average or slightly higher is
+# generally considered acceptable. If lower, then it indicates a higher risk of
+# distress or default. If it is very high, it indicates management may not be 
+# using its assets efficiently.
+
+
+#Dependent Variable
+#dividend: 1 if a stock pays dividend
+
+#Want to figure out if a stock pays dividend or not. 
+
+
+# PC Analysis-----------------------------------------------------------
+pairs.panels(df[,-1], gap = 0, 
+             bg = c("red", "blue")[df$dividend], pch = 21 )
+
+#Based off the plot, we may see a slightly higher level of correlation between 
+#current_ratio and mcap. PCA may aid in dealing with this situation. 
+
+
+#PCA Analysis 
+#We first need to scale the ind. variables, as they each are measured quite 
+#differently.
+pc = prcomp(df[,-1], center = TRUE, scale. = TRUE)
+attributes(pc)
+pc
+summary(pc)
+
+pairs.panels(pc$x, gap = 0, 
+             bg = c("red", "blue")[t=df$dividend], pch = 21)
+
+
+#Bi-Plot
+g <- ggbiplot(pc, choices = 3:4,
+              obs.scale = 1,
+              var.scale = 1,
+              groups = df$dividend,
+              ellipse = TRUE,
+              circle = TRUE,
+              ellipse.prob = 0.68)
+g <- g + scale_color_discrete(name = '')
+g <- g + theme(legend.direction = 'horizontal',
+               legend.position = 'top')
+print(g)
+
+#Scree Plot
+var_exp = pc$sdev^2/sum(pc$sdev^2)
+var_exp
+
+
+plot(var_exp, type = 'b', col = 'green', yaxt = 'n', ylab = '', xlab = '')
+par(new = T)
+plot(cumsum(var_exp), xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     main = "Scree Plot Using Correlation Matrix",
+     ylim = c(0, 1), type = "b", col = 'blue')
+legend(4.2, 0.4, legend=c("Proportion", "Cumulative"),
+       col=c("green", "blue"), lty=1:1, cex=0.8)
+
+# Seems like 4th PC components will be useful for our data.
+
+
+# New Data set with PC ----------------------------------------------------
+df_pc = pc$x[,1:3]
+df_pc = cbind(df_pc, df0$dividend)
+colnames(df_pc)[4] = "dividend"
+df_pc = as.data.frame(df_pc)
+df_pc$dividend = factor(df_pc$dividend)
+
+
+# LDA ---------------------------------------------------------------------
+
+####### Linear discriminant model Using the PC
+
+#Linear discriminant model
+model1 = lda( dividend ~ PC1 + PC2 + PC3, data = df_pc)
+model1
+
+
+# Cross Validation 5 times where 80% is used as the training data
+set.seed(2)
+rep = 5
+errlin = dim(rep)
+for (i in 1: rep){
+  training = sample(1:200, 165)
+  trg = df_pc[training,]
+  tst = df_pc[-training,]
+  mod = lda(dividend ~ PC1 + PC2 + PC3, data = trg)
+  pred = predict(mod, tst)$class
+  tablin = table(tst$dividend, pred)
+  errlin[i] = (dim(tst)[1] - sum(diag(tablin)))/dim(tst)[1]
+}
+merrlin = mean(errlin)
+merrlin
+
+#We have a miss-classification rate of 8%
+
+
+
+
+#######Performing LDA without PCA
+
+#Linear discriminant model
+mod = lda( dividend ~., data = df)
+mod
+
+
+# Cross Validation 5 times where 80% is used as the training data.
+set.seed(3)
+rep = 5
+errlin = dim(rep)
+for (i in 1: rep){
+  training = sample(1:200, 165)
+  trg = df[training,]
+  tst = df[-training,]
+  mod = lda(dividend ~., data = trg)
+  pred = predict(mod, tst)$class
+  tablin = table(tst$dividend, pred)
+  errlin[i] = (dim(tst)[1] - sum(diag(tablin)))/dim(tst)[1]
+}
+merrlin = mean(errlin)
+merrlin
+
+#We have a miss-classification rate of 4%
+
+
+
+
+
+
+
+# Vi's Work ---------------------------------------------------------------
+#pca, scree plot
+myPr=prcomp(df[,-1], scale=TRUE )
+plot(myPr, type="l")
+
+#
+myPr
+summary(myPr)
+
+#red arrows are the eigen vectors of each variable in df, 
+#each numbers are our obs from df(in order), and are plotted by its values of PC1 and PC 2
+#interpretation: _an obs has higher value of PC1, it has lower value in earnings_growth,slightly lower in current_ratio, fcfps and mcap, 
+#and it has higher value of de
+#                _an obs has higher value on PC2, it has lower value in earnings_growth,slightly lower in de 
+#and no real  difference in mcap and fcfps
+#and slightly higher in current_ratio
+#biplot helps with understanding the relationship bw the variables and the PCs
+biplot(myPr, scale = 0)
+
+#components of myPr
+str(myPr)
+#attach PC1 and PC2 scores into our df 
+newdf=cbind(df, myPr$x[,1:2])
+
+#gg plot
+
+
+
+
+
+
+
+library("factoextra")
+
+data("decathlon2")
+
+View(decathlon2)
